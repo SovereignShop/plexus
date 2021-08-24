6343053546cab876ae74b8c9f14546b13c03475f
John Collins <john.collins@braincorp.com>
2021-08-24 14:43:25 -0700

    Simple impl

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scad-paths
-An OpenSCAD paths library
+
+An (scad-clj)[https://github.com/farrellm/scad-clj] paths library. It defines a simple language for extruding shapes in a step-wise, relative fashion using egocentric reference frames. Reference frames are currently 2D (x,y,theta), but will be extended to support 3D paths.
--- src/main/scad_paths/core.clj
+++ src/main/scad_paths/core.clj
@@ -1,13 +1,13 @@
 (ns scad-paths.core
   (:require
    [scad-paths.utils :as u]
-   [scad-paths.pipes :as pipes]
    [scad-clj.model :as m]))
 
-(defmulti path-segment (fn [op pose args] op))
+(defmulti path-segment (fn [op pose segments args] op))
 
-(defn path [& path]
-  (let [curr-args {:or 10 :ir 7 :shell 1 :fn 100}]
+(defn path [args & path]
+  (let [curr-args (into {:or 10 :curve-radius 7 :shell 1/2 :fn 100 :shape (u/circle-shell 3 2)}
+                        args)]
     (loop [pose [0 0 0]
            [[op & opts] & steps] path
            args curr-args
@@ -15,55 +15,90 @@
       (if (nil? op)
         (m/union ret)
         (let [options (into args (apply hash-map opts))
-              [new-pose part] (path-segment op pose options)]
-          (recur new-pose steps options (conj ret part)))))))
+              [new-pose parts] (path-segment op pose ret options)]
+          (recur new-pose steps options parts))))))
 
-(defmethod path-segment :pipe/left
-  [_ [x y angle] {:keys [or ir shell fn]}]
-  (let [tr (+ ir (u/half (- or ir)))
-        part (binding [m/*fn* fn]
-               (->> (pipes/quarter-pipe or ir :shell shell)
-                    (m/translate [(- tr) 0 0])
+(defmethod path-segment :extrude/left
+  [_ [x y angle] segments {:keys [curve-radius fn shape]}]
+  (let [part (binding [m/*fn* fn]
+               (->> shape
+                    (m/translate [curve-radius 0 0])
+                    (m/extrude-rotate {:angle 90})
+                    (m/translate [(- curve-radius) 0 0])
                     (m/rotatec [0 0 (- angle)])
                     (m/translate [x y 0])))
         new-angle (- angle (/ u/pi 2))]
     [[(+ x
-         (* tr (Math/sin angle))
-         (* tr (Math/sin new-angle)))
+         (* curve-radius (Math/sin angle))
+         (* curve-radius (Math/sin new-angle)))
       (+ y
-         (* tr (Math/cos angle))
-         (* tr (Math/cos new-angle)))
+         (* curve-radius (Math/cos angle))
+         (* curve-radius (Math/cos new-angle)))
       new-angle]
-     part]))
+     (conj segments part)]))
 
-
-(defmethod path-segment :pipe/right
-  [_ [x y angle] {:keys [or ir shell fn]}]
-  (let [tr (+ ir (u/half (- or ir)))
-        part (binding [m/*fn* fn]
-               (->> (pipes/quarter-pipe or ir :shell shell)
-                    (m/translate [(- tr) 0 0])
+(defmethod path-segment :extrude/right
+  [_ [x y angle] segments {:keys [curve-radius fn shape]}]
+  (let [part (binding [m/*fn* fn]
+               (->> shape
+                    (m/translate [curve-radius 0 0])
+                    (m/extrude-rotate {:angle 90})
+                    (m/translate [(- curve-radius) 0 0])
                     (m/rotatec [0 u/pi 0])
                     (m/rotatec [0 0 (- angle)])
                     (m/translate [x y 0])))
         new-angle (+ angle (/ u/pi 2))]
     [[(+ x
-         (* tr (Math/sin angle))
-         (* tr (Math/sin new-angle)))
+         (* curve-radius (Math/sin angle))
+         (* curve-radius (Math/sin new-angle)))
       (+ y
-         (* tr (Math/cos angle))
-         (* tr (Math/cos new-angle)))
+         (* curve-radius (Math/cos angle))
+         (* curve-radius (Math/cos new-angle)))
       new-angle]
-     part]))
+     (conj segments part)]))
 
-(defmethod path-segment :pipe/straight
-  [_ [x y angle] {:keys [or ir shell fn length] :or {length 10}}]
-  (let [thickness (- or ir)
-        part (binding [m/*fn* fn]
-               (->> (pipes/pipe thickness (- thickness (* 2 shell)) length :center false)
+(defmethod path-segment :extrude/forward
+  [_ [x y angle] segments {:keys [fn length shape] :or {length 10}}]
+  (let [part (binding [m/*fn* fn]
+               (->> shape
+                    (m/extrude-linear {:height length :center false})
+                    (m/rotatec [(- (u/half u/pi)) 0 0])
                     (m/rotatec [0 0 (- angle)])
                     (m/translate [x y 0])))]
     [[(+ x (* length (Math/sin angle)))
       (+ y (* length (Math/cos angle)))
       angle]
-     part]))
+     (conj segments part)]))
+
+(defmethod path-segment :extrude/hull
+  [_ [x y angle] segments {:keys [fn mask-shape shape]}]
+
+  (let [part (binding [m/*fn* fn]
+               (->> shape
+                    (m/extrude-linear {:height 0.1 :center false})
+                    (m/rotatec [(- (u/half u/pi)) 0 0])
+                    (m/rotatec [0 0 (- angle)])
+                    (m/translate [x y 0])))
+        mask-part (when mask-shape
+                    (binding [m/*fn* fn]
+                      (->> mask-shape
+                           (m/extrude-linear {:height 0.1 :center false})
+                           (m/rotatec [(- (u/half u/pi)) 0 0])
+                           (m/rotatec [0 0 (- angle)])
+                           (m/translate [x y 0]))))]
+    [[x y angle]
+     (conj (pop segments)
+           (cond-> (m/hull (peek segments) part)
+             mask-part (m/difference (m/hull (peek segments) mask-part))))]))
+
+(defn left [& opts]
+  `(:extrude/left ~@opts))
+
+(defn right [& opts]
+  `(:extrude/right ~@opts))
+
+(defn forward [& opts]
+  `(:extrude/forward ~@opts))
+
+(defn hull [& opts]
+  `(:extrude/hull ~@opts))
