f0746bbce50551e0aae877934c2e2d94d24d0b97
John Collins <john.collins@braincorp.com>
2021-08-26 10:59:00 -0700

    Branching and context

--- src/main/scad_paths/core.clj
+++ src/main/scad_paths/core.clj
@@ -3,24 +3,27 @@
    [scad-paths.utils :as u]
    [scad-clj.model :as m]))
 
-(defmulti path-segment (fn [op pose segments args] op))
+(defmulti path-segment (fn [ctx _ _ _] (:op ctx)))
 
-(defn path [args & path]
-  (let [curr-args (into {:or 10 :curve-radius 7 :shell 1/2 :fn 100 :shape (u/circle-shell 3 2)}
-                        args)]
+(defn path [ctx & path]
+  (let [curr-context (into {:or 10 :curve-radius 7 :shell 1/2 :fn 100 :shape (u/circle-shell 3 2)}
+                           ctx)]
     (loop [pose [0 0 0]
-           [[op & opts] & steps] path
-           args curr-args
+           [[op & args] & steps] path
+           ctx curr-context
            ret []]
       (if (nil? op)
         (m/union ret)
-        (let [options (into args (apply hash-map opts))
-              [new-pose parts] (path-segment op pose ret options)]
-          (recur new-pose steps options parts))))))
+        (if (= op ::context)
+          (recur pose steps (into ctx (partition 2 args)) ret)
+          (let [[new-pose parts] (path-segment (assoc ctx :op op) pose ret args)]
+            (recur new-pose steps ctx parts)))))))
 
 (defmethod path-segment ::left
-  [_ [x y angle] segments {:keys [curve-radius fn shape gap]}]
-  (let [part (binding [m/*fn* fn]
+  [{:keys [fn shape gap] :as ctx} [x y angle] segments args]
+  (let [[& {:keys [curve-radius]
+            :or {curve-radius (:curve-radius ctx)}}] args
+        part (binding [m/*fn* fn]
                (->> shape
                     (m/translate [curve-radius 0 0])
                     (m/extrude-rotate {:angle 90})
@@ -36,8 +39,10 @@
        (conj segments part))]))
 
 (defmethod path-segment ::right
-  [_ [x y angle] segments {:keys [curve-radius fn shape gap]}]
-  (let [part (binding [m/*fn* fn]
+  [{:keys [fn shape gap] :as ctx} [x y angle] segments args]
+  (let [[& {:keys [curve-radius]
+            :or {curve-radius (:curve-radius ctx)}}] args
+        part (binding [m/*fn* fn]
                (->> shape
                     (m/translate [curve-radius 0 0])
                     (m/extrude-rotate {:angle 90})
@@ -54,8 +59,10 @@
        (conj segments part))]))
 
 (defmethod path-segment ::curve
-  [_ [x y angle] segments {:keys [curve-radius curve-angle fn shape gap]}]
-  (let [part (binding [m/*fn* fn]
+  [{:keys [fn shape gap] :as ctx} [x y angle] segments args]
+  (let [[& {:keys [curve-radius curve-angle]
+            :or {curve-radius (:curve-radius ctx)}}] args
+        part (binding [m/*fn* fn]
                (if (pos? curve-angle)
                  (->> shape
                       (m/translate [curve-radius 0 0])
@@ -81,7 +88,7 @@
        (conj segments part))]))
 
 (defmethod path-segment ::forward
-  [_ [x y angle] segments {:keys [fn length shape gap model] :or {length 10}}]
+  [{:keys [fn shape gap]} [x y angle] segments [& {:keys [length model]}]]
   (let [part (binding [m/*fn* fn]
                (->> (if model
                       model
@@ -98,12 +105,21 @@
        (conj segments part))]))
 
 (defmethod path-segment ::hull
-  [_ [x y angle] segments {:keys [fn]}]
+  [{:keys [fn]} pose segments _]
   (let [part (binding [m/*fn* fn]
                (m/hull (-> segments pop peek)
                        (peek segments)))]
-    [[x y angle]
+    [pose
      (conj (-> segments pop pop) part)]))
+
+(defmethod path-segment ::branch
+  [ctx [x y angle :as pose] segments args]
+  (let [model (apply path ctx args)]
+    [pose
+     (conj segments
+           (->> model
+                (m/rotatec [0 0 (- angle)])
+                (m/translate [x y 0])))]))
 
 (defn left [& opts]
   `(::left ~@opts))
@@ -119,3 +135,9 @@
 
 (defn hull [& opts]
   `(::hull ~@opts))
+
+(defn branch [& args]
+  `(::branch ~@args))
+
+(defn context [& args]
+  `(::context ~@args))
