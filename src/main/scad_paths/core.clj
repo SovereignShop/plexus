77c00eef67ffdba64d403785b2dbd1557edb6947
John Collins <john.collins@braincorp.com>
2021-09-08 23:58:59 -0700

    Cleanup

--- src/main/scad_paths/core.clj
+++ src/main/scad_paths/core.clj
@@ -1,137 +1,14 @@
 (ns scad-paths.core
   (:require
-   [clj-tf.utils :as tf]
    [scad-paths.utils :as u]
    [scad-clj.model :as m]))
 
-(defn rotation-vector
-  "Rodrigues rotation formula"
-  [v k a]
-  (tf/add (tf/mul (vec v)
-                  (Math/cos a))
-          (tf/mul (tf/cross k v)
-                  (Math/sin a))
-          (tf/mul (vec k)
-                  (tf/dot k v)
-                  (- 1 (Math/cos a)))))
-
-(defn turn-left
-  ([v]
-   (turn-left v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [(rotation-vector vx vz a)
-    (rotation-vector vy vz a)
-    vz]))
-
-(defn turn-right
-  ([v]
-   (turn-right v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [(rotation-vector vx vz (- a))
-    (rotation-vector vy vz (- a))
-    vz]))
-
-(defn turn-up
-  ([v]
-   (turn-up v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [vx
-    (rotation-vector vy vx a)
-    (rotation-vector vz vx a)]))
-
-(defn turn-down
-  ([v]
-   (turn-down v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [vx
-    (rotation-vector vy vx (- a))
-    (rotation-vector vz vx (- a))]))
-
-(defn roll-left
-  ([v]
-   (roll-left v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [(rotation-vector vx vy a)
-    vy
-    (rotation-vector vz vy a)]))
-
-(defn roll-right
-  ([v]
-   (roll-left v (/ Math/PI 2)))
-  ([[vx vy vz] a]
-   [(rotation-vector vx vy (- a))
-    vy
-    (rotation-vector vz vy (- a))]))
-
-(defn rotate
-  [[vx vy vz] axis a]
-  [(rotation-vector vx axis a)
-   (rotation-vector vy axis a)
-   (rotation-vector vz axis a)])
-
-(defn angle-between [a b]
-  (Math/acos (/ (tf/dot a b) (* (tf/mag a) (tf/mag b)))))
-
-(defn go-forward [p v x]
-  (tf/add p (tf/mul (second v) x)))
-
-(defn about-equal? [v1 v2]
-  (loop [[x & xs] v1
-         [y & ys] v2]
-    (cond
-      (nil? x) true
-
-      (< (Math/abs (- x y)) 0.00001)
-      (recur xs ys)
-
-      :else false)))
-
-(defn opposing? [v1 v2]
-  (about-equal? (tf/add v1 v2) [0 0 0]))
-
-(defn ortho-angle [v1 v2 opp]
-  (let [cross (tf/cross v1 v2)]
-    (if (about-equal? cross [0 0 0])
-      (if (opposing? v1 v2)
-        [Math/PI opp]
-        [0 opp])
-      [(angle-between v1 v2) (tf/normalise cross)])))
-
-(defn about= [a b]
-  (< (Math/abs (- a b))
-     0.00001))
-
-(defn scad-transform
-  ([m pose] (scad-transform [[1 0 0] [0 1 0] [0 0 1]] m pose))
-  ([a b pose]
-   (let [[angle ortho] (ortho-angle (nth a 0) (nth b 0) [0 0 1])
-         c (rotate a ortho angle)
-         [angle-2 ortho-2] (ortho-angle (nth b 1) (nth c 1) (nth c 0))]
-     (comp
-      (partial m/translate pose)
-      (partial m/rotatev (- angle-2) ortho-2)
-      (partial m/rotatev angle ortho)))))
-
 (defmulti path-segment (fn [ctx _] (:op ctx)))
 
-(defn path [ctx & path]
-  (let [curr-context (into {:or 10 :curve-radius 7 :shell 1/2 :fn 100 :shape (u/circle-shell 3 2)}
-                           ctx)]
-    (loop [pose [0 0 0]
-           [[op & args] & steps] path
-           ctx curr-context
-           ret []]
-      (if (nil? op)
-        (m/union ret)
-        (if (= op ::context)
-          (recur pose steps (into ctx (partition-all 2) (vec args)) ret)
-          (let [[new-pose parts] (path-segment (assoc ctx :op op) pose ret args)]
-            (recur new-pose steps ctx parts)))))))
-
-(defn path-grid
-  ([ctx path]
-   (path-grid ctx ctx path))
-  ([outer-context inner-context path]
+(defn path
+  ([ctx path-spec]
+   (path ctx ctx path-spec))
+  ([outer-context inner-context path-spec]
    (let [default-context {:or 10
                           :curve-radius 7
                           :shell 1/2
@@ -145,7 +22,7 @@
          merge-fn (partial merge-with into)]
      (loop [ret {:outer-context (into default-context outer-context)
                  :inner-context (into default-context inner-context)}
-            [seg & segments] path]
+            [seg & segments] path-spec]
        (let [[l r] (if (vector? seg) seg [seg])
              outer-context (:outer-context ret)
              inner-context (:inner-context ret)]
@@ -154,14 +31,14 @@
 
                (= l :branch)
                (let [ret (merge-fn ret
-                                   (-> (path-grid outer-context inner-context r)
+                                   (-> (path outer-context inner-context r)
                                        (dissoc :outer-context :inner-context)))]
                  (recur ret segments))
 
                (= l :model)
                (let [k (first r)
                      p (next r)
-                     r (path-grid outer-context inner-context p)]
+                     r (path outer-context inner-context p)]
                  (recur (-> (merge-fn ret r)
                             (assoc-in [:models k] r))
                         segments))
@@ -169,7 +46,7 @@
                (= l :extra-model)
                (let [k (first r)
                      p (next r)
-                     r (path-grid outer-context inner-context p)]
+                     r (path outer-context inner-context p)]
                  (recur (-> ret
                             (assoc-in [:models k] r))
                         segments))
@@ -188,92 +65,95 @@
                  (recur (assoc ret :outer-context new-outer-ctx :inner-context new-inner-ctx)
                         segments))))))))
 
-(defn bAc->a
-  "side,angle,side -> side via. law of cosines."
-  [b A c]
-  (Math/sqrt (- (+ (Math/pow b 2)
-                   (Math/pow c 2))
-                (* 2 b c (Math/cos A)))))
-
 (defmethod path-segment ::left
-  [{:keys [fn shape gap pose axes rot] :as ctx} args]
-  (let [[& {:keys [curve-radius]
-            :or {curve-radius (:curve-radius ctx)}}] args
-        transform (scad-transform axes pose)
+  [{:keys [fn shape gap pose axes] :as ctx} args]
+  (let [[& {:keys [curve-radius angle]
+            :or {curve-radius (:curve-radius ctx)
+                 angle (/ Math/PI 2)}}] args
+        degrees (* angle 57.29578)
+        transform (u/->scad-transform axes pose)
         part (binding [m/*fn* fn]
                (->> shape
                     (m/translate [curve-radius 0 0])
-                    (m/extrude-rotate {:angle 90})
+                    (m/extrude-rotate {:angle degrees})
                     (m/translate [(- curve-radius) 0 0])
                     transform))
-        new-pose (go-forward pose axes curve-radius)
-        new-axes (turn-left axes)
-        new-pose (go-forward new-pose new-axes curve-radius)]
+        d (u/bAc->a curve-radius angle curve-radius)
+        r (- (/ Math/PI 2) (/ (- Math/PI angle) 2))
+        new-axes (u/yaw axes r)
+        new-pose (u/go-forward pose new-axes d)
+        new-axes (u/yaw new-axes (- angle r))]
     (cond-> (assoc ctx :pose new-pose :axes new-axes)
       (not gap) (update :form conj part))))
 
 (defmethod path-segment ::right
-  [{:keys [fn shape gap pose axes rot] :as ctx} args]
-  (let [[& {:keys [curve-radius]
-            :or {curve-radius (:curve-radius ctx)}}] args
-        rt       (comp
-                  (partial m/translate [curve-radius curve-radius 0])
-                  (partial m/rotatec [0 0 (- (/ Math/PI 2))])
-                  rot)
-        transform (scad-transform axes pose)
+  [{:keys [fn shape gap pose axes] :as ctx} args]
+  (let [[& {:keys [curve-radius angle]
+            :or {curve-radius (:curve-radius ctx)
+                 angle (/ Math/PI 2)}}] args
+        degrees (* angle 57.29578)
+        transform (u/->scad-transform axes pose)
         part (binding [m/*fn* fn]
                (->> shape
                     (m/translate [curve-radius 0 0])
-                    (m/extrude-rotate {:angle 90})
+                    (m/extrude-rotate {:angle degrees})
                     (m/translate [(- curve-radius) 0 0])
                     (m/rotatec [0 u/pi 0])
                     transform))
-        new-pose (go-forward pose axes curve-radius)
-        new-axes (turn-right axes)
-        new-pose (go-forward new-pose new-axes curve-radius)]
-    (cond-> (assoc ctx :pose new-pose :axes new-axes :rot rt)
-      (not gap) (update :form conj part))))
-
-(defmethod path-segment ::up
-  [{:keys [fn shape gap pose axes rot] :as ctx} args]
-  (let [[& {:keys [curve-radius angle]
-            :or {curve-radius (:curve-radius ctx)}}] args
-        transform (scad-transform axes pose)
-        part (binding [m/*fn* fn]
-               (->> shape
-                    (m/translate [curve-radius 0 0])
-                    (m/extrude-rotate {:angle 90})
-                    (m/translate [(- curve-radius) 0 0])
-                    (m/rotatec [0 (/ u/pi 2) 0])
-                    transform))
-        new-pose (go-forward pose axes curve-radius)
-        new-axes (turn-up axes (or angle (/ Math/PI 2)))
-        new-pose (go-forward new-pose new-axes curve-radius)]
+        d (u/bAc->a curve-radius angle curve-radius)
+        r (- (/ Math/PI 2) (/ (- Math/PI angle) 2))
+        new-axes (u/yaw axes (- r))
+        new-pose (u/go-forward pose new-axes d)
+        new-axes (u/yaw new-axes (- (- angle r)))]
     (cond-> (assoc ctx :pose new-pose :axes new-axes)
       (not gap) (update :form conj part))))
 
+(defmethod path-segment ::up
+  [{:keys [fn shape gap pose axes] :as ctx} args]
+  (let [[& {:keys [curve-radius angle]
+            :or {curve-radius (:curve-radius ctx)
+                 angle (/ Math/PI 2)}}] args
+        transform (u/->scad-transform axes pose)
+        degrees (* angle 57.29578)
+        part (binding [m/*fn* fn]
+               (->> shape
+                    (m/translate [curve-radius 0 0])
+                    (m/extrude-rotate {:angle degrees})
+                    (m/translate [(- curve-radius) 0 0])
+                    (m/rotatec [0 (/ u/pi 2) 0])
+                    transform))
+        d (u/bAc->a curve-radius angle curve-radius)
+        r (- (/ Math/PI 2) (/ (- Math/PI angle) 2))
+        new-axes (u/pitch axes r)
+        new-pose (u/go-forward pose new-axes d)
+        new-axes (u/pitch new-axes (- angle r))]
+    (cond-> (assoc ctx :pose new-pose :axes new-axes)
+      (not gap) (update :form conj part))))
 
 (defmethod path-segment ::down
   [{:keys [fn shape gap pose axes rot] :as ctx} args]
   (let [[& {:keys [curve-radius angle]
             :or {curve-radius (:curve-radius ctx)}}] args
-        transform (scad-transform axes pose)
+        transform (u/->scad-transform axes pose)
+        degrees  (* angle 57.29578)
         part (binding [m/*fn* fn]
                (->> shape
                     (m/translate [curve-radius 0 0])
-                    (m/extrude-rotate {:angle 90})
+                    (m/extrude-rotate {:angle degrees})
                     (m/translate [(- curve-radius) 0 0])
                     (m/rotatec [0 (/ u/pi 2) 0])
                     transform))
-        new-pose (go-forward pose axes curve-radius)
-        new-axes (turn-down axes (or angle (/ Math/PI 2)))
-        new-pose (go-forward new-pose new-axes curve-radius)]
+        d (u/bAc->a curve-radius angle curve-radius)
+        r (- (/ Math/PI 2) (/ (- Math/PI angle) 2))
+        new-axes (u/pitch axes (- r))
+        new-pose (u/go-forward pose new-axes d)
+        new-axes (u/pitch new-axes (- (- angle r)))]
     (cond-> (assoc ctx :pose new-pose :axes new-axes)
       (not gap) (update :form conj part))))
 
 (defmethod path-segment ::forward
   [{:keys [fn shape pose axes] :as ctx} [& {:keys [length model twist gap mask]}]]
-  (let [transform (scad-transform axes pose)
+  (let [transform (u/->scad-transform axes pose)
         part (binding [m/*fn* fn]
                (as-> (if model
                        model
@@ -284,26 +164,22 @@
                    (m/difference m mask)
                    m)
                  (transform m)))
-        new-pose (go-forward pose axes length)]
+        new-pose (u/go-forward pose axes length)]
     (cond-> (assoc ctx :pose new-pose)
       (not gap) (update :form conj part))))
 
-#_(defmethod path-segment ::hull
-  [{:keys [fn]} pose segments _]
-  (let [part (binding [m/*fn* fn]
-               (m/hull (-> segments pop peek)
-                       (peek segments)))]
-    [pose
-     (conj (-> segments pop pop) part)]))
+(defmethod path-segment ::roll
+  [{:keys [axes] :as ctx} [& {:keys [angle] :or {angle (/ Math/PI 2)}}]]
+  (let [new-axes (u/roll axes angle)]
+    (assoc ctx :axes new-axes)))
 
-#_(defmethod path-segment ::branch
-  [ctx [x y angle :as pose] segments args]
-  (let [model (apply path ctx args)]
-    [pose
-     (conj segments
-           (->> model
-                (m/rotatec [0 0 (- angle)])
-                (m/translate [x y 0])))]))
+(defmethod path-segment ::hull
+  [{:keys [fn form] :as ctx} [& {:keys [n-segments] :or {n-segments 2}}]]
+  (let [hull-forms (into () (take n-segments) (map peek (iterate pop form)))
+        other-forms (nth (iterate pop form) n-segments)
+        part (binding [m/*fn* fn]
+               (m/hull hull-forms))]
+    (assoc ctx :form (conj other-forms part))))
 
 (defn left [& opts]
   `(::left ~@opts))
@@ -314,8 +190,11 @@
 (defn up [& opts]
   `(::up ~@opts))
 
-(defn curve [& opts]
-  `(::curve ~@opts))
+(defn down [& opts]
+  `(::down ~@opts))
+
+(defn roll [& opts]
+  `(::roll ~@opts))
 
 (defn forward [& opts]
   `(::forward ~@opts))
@@ -323,18 +202,17 @@
 (defn hull [& opts]
   `(::hull ~@opts))
 
-(defn branch [& args]
-  `(::branch ~@args))
-
 (defn context [& args]
   `(::context ~@args))
 
 (defn model [& args]
   `(::model ~@args))
 
+(comment
 
-(->
- (path-grid {}
-            [(left) (up) (right) (forward :length 10) (right) (right) (left) (forward :length 30) (right) (up)
-             (forward :length 100)])
- :outer-context :form m/union)
+  (->
+   (path {:curve-radius 20}
+         [(left :angle (/ Math/PI 1.5)) (up) (right :angle (/ Math/PI 6)) (roll) (forward :length 10) (right) (right) (hull :n-segments 4) (left) (forward :length 30) (right) (up) (forward :length 100)])
+   :outer-context :form m/union)
+
+  )
--- src/main/scad_paths/utils.clj
+++ src/main/scad_paths/utils.clj
@@ -1,213 +1,96 @@
 (ns scad-paths.utils
   (:require
-   [clojure.string :as string]
-   [scad-clj.model :as m]
-   [scad-clj.scad :as s]))
+   [clj-tf.utils :as tf]
+   [scad-clj.model :as m]))
 
-(defn half [x] (/ x 2))
 (def pi Math/PI)
-(def hpi (/ Math/PI 2))
-(def qpi (/ Math/PI 4))
-(def opi (/ Math/PI 8))
 
-(defn semi-circle [r angle]
-  (let [n-steps (or m/*fn* 10)
-        step-size (/ angle n-steps)]
-    (m/polygon
-     (cons [0 0]
-           (for [step (range (inc n-steps))]
-             (let [d (* step step-size)]
-               [(* r (Math/cos d)) (* r (Math/sin d))]))))))
+(defn rotation-vector
+  "Rodrigues rotation formula"
+  [v k a]
+  (tf/add (tf/mul (vec v)
+                  (Math/cos a))
+          (tf/mul (tf/cross k v)
+                  (Math/sin a))
+          (tf/mul (vec k)
+                  (tf/dot k v)
+                  (- 1 (Math/cos a)))))
 
-(defn cone [top-radius bottom-radius height]
-  (m/hull (m/cylinder bottom-radius 1.0 :center false)
-          (->> (m/cylinder top-radius 1 :center false)
-               (m/translate [0 0 (- height 1)]))))
+(defn yaw
+  ([v]
+   (yaw v (/ pi 2)))
+  ([[vx vy vz] a]
+   [(rotation-vector vx vz a)
+    (rotation-vector vy vz a)
+    vz]))
 
-(defn pyramid
-  ([top-size bottom-size height]
-   (pyramid top-size bottom-size height 1))
-  ([top-size bottom-size height bottom-thickness]
-   (m/hull (->> (m/square top-size top-size)
-                (m/extrude-linear {:height bottom-thickness :center false}))
-           (->> (m/square bottom-size bottom-size)
-                (m/extrude-linear {:height bottom-thickness :center false})
-                (m/translate [0 0 (- height bottom-thickness)])))))
+(defn pitch
+  ([v]
+   (pitch v (/ pi 2)))
+  ([[vx vy vz] a]
+   [vx
+    (rotation-vector vy vx a)
+    (rotation-vector vz vx a)]))
 
-(defn square-circle-hull
-  ([bottom-size top-radius height]
-   (square-circle-hull bottom-size top-radius height 1))
-  ([bottom-size top-radius height bottom-thickness]
-   (m/hull
-    (->> (m/circle top-radius)
-         (m/extrude-linear {:height bottom-thickness :center false})
-         (m/translate [0 0 (- height bottom-thickness)]))
-    (->> (m/square bottom-size bottom-size)
-         (m/extrude-linear {:height bottom-thickness :center false})))))
+(defn roll
+  ([v]
+   (roll v (/ pi 2)))
+  ([[vx vy vz] a]
+   [(rotation-vector vx vy a)
+    vy
+    (rotation-vector vz vy a)]))
 
-(defn circle-square-hull
-  ([bottom-size top-radius height]
-   (square-circle-hull bottom-size top-radius height 1))
-  ([bottom-size top-radius height bottom-thickness]
-   (m/hull (->> (m/square bottom-size bottom-size)
-                (m/extrude-linear {:height bottom-thickness :center false})
-                (m/translate [0 0 (- height bottom-thickness)]))
-           (->> (m/circle top-radius)
-                (m/extrude-linear {:height bottom-thickness :center false})))))
+(defn go-forward [p v x]
+  (tf/add p (tf/mul (second v) x)))
 
-(defn helix
-  [shape steps height twist dir]
-  (let [shape-3d (->> shape (m/extrude-linear {:height 0.1}))
-        section (m/hull
-                 (for [step (range 2)]
-                   (->> shape-3d
-                        (m/translate [0 0 (* step  (/ height steps))])
-                        (m/rotatec [0 0 (* step (case dir :clockwise 1 -1) (/ twist steps))]))))]
-    (m/union
-     section
-     (for [step (range 1 steps)]
-       (->> section
-            (m/translate [0 0 (* step (/ height steps))])
-            (m/rotatec [0 0 (* step (case dir :clockwise 1 -1) (/ twist steps))]))))))
+(defn rotate
+  [[vx vy vz] axis a]
+  [(rotation-vector vx axis a)
+   (rotation-vector vy axis a)
+   (rotation-vector vz axis a)])
 
-(defn helix-extrude
-  " shape_pts : A list of points represent a shape. See the example below.
-       radius : The radius of the cylinder. The radius of the cylinder. It also accepts a vector [r1, r2]. r1 is the bottom radius and r2 is the top radius of a cone.
-       levels : The level count is performed every 360 degrees.
-   level_dist : The distance between two vertial points.
-       vt_dir : \"SPI_DOWN\" for spiraling down. \"SPI_UP\" for spiraling up. The default value is \"SPI_DOWN\".
-       rt_dir : \"CT_CLK\" for counterclockwise. \"CLK\" for clockwise. The default value is \"CT_CLK\".
-        twist : The number of degrees of through which the shape is extruded.
-        scale : Scales the 2D shape by this value over the length of the extrusion. Scale can be a scalar or a vector.
-    triangles : \"SOLID\" (default), \"HOLLOW\" or user-defined indexes. See polysections for details.
-$fa, $fs, $fn : Check the cylinder module for more details."
-  [& {:keys [shape-pts
-             radius
-             levels
-             level-dist
-             vt-dir
-             rt-dir
-             twist
-             scale
-             triangles]
-      :or {vt-dir "SPI_DOWN"
-           rt-dir "CT_CLK"
-           twist 180
-           scale 1
-           triangles "SOLID"}}]
-  `(:helix-extrude ~shape-pts ~radius ~levels ~level-dist ~vt-dir ~rt-dir ~twist ~scale ~triangles))
+(defn bAc->a
+  "side,angle,side -> side via. law of cosines."
+  [b A c]
+  (Math/sqrt (- (+ (Math/pow b 2)
+                   (Math/pow c 2))
+                (* 2 b c (Math/cos A)))))
 
-(defn circle-shell
-  ([or ir]
-   (m/difference (m/circle or) (m/circle ir)))
-  ([or ir & {:keys [height center offset] :or {height 1 center false offset 0}}]
-   (m/difference
-    (m/cylinder or height :center center)
-    (->> (m/cylinder ir height :center center)
-         (m/translate [0 0 offset])))))
+(defn angle-between [a b]
+  (Math/acos (/ (tf/dot a b) (* (tf/mag a) (tf/mag b)))))
 
-(defn square
-  [x y & {:keys [fillet center] :or {center true fillet 0}}]
-  (if (pos? fillet)
-    (->> (m/square (- x (* 2 fillet)) (- y (* 2 fillet)) :center center)
-         (m/minkowski (m/circle fillet)))
-    (m/square x y :center center)))
+(defn about-equal? [v1 v2]
+  (loop [[x & xs] v1
+         [y & ys] v2]
+    (cond
+      (nil? x) true
 
-(defn square-shell
-  ([x y t & {:keys [center height offset] :or {center true offset 0}}]
-   (cond->> (m/difference (m/square x y :center center)
-                          (m/square (- x (* 2 t)) (- y (* 2 t)) :center center))
-     height (m/extrude-linear {:height height :center false}) )))
+      (< (Math/abs (- x y)) 0.00001)
+      (recur xs ys)
 
-(defn torus
-  ([or ir & {:keys [angle shell] :or {angle 360}}]
-   (let [thickness (- or ir)]
-     (cond-> (->> (m/circle thickness)
-                  (m/translate [(+ ir (half thickness)) 0 0])
-                  (m/extrude-rotate {:angle angle}))
-       shell (m/difference (torus (- or shell) (+ ir shell) :angle angle))))))
+      :else false)))
 
-(defn half-sphere [r]
-  (m/difference (m/sphere r)
-                (->> (m/cube (* 2 r) (* 2 r) (* 2 r))
-                     (m/translate [0 0 (- r)]))))
+(defn opposing? [v1 v2]
+  (about-equal? (tf/add v1 v2) [0 0 0]))
 
-(defn hull
-  [bottom-shape top-shape height]
-  (m/hull (m/extrude-linear {:height 0.1 :center false} bottom-shape)
-          (->> top-shape
-               (m/extrude-linear {:height 0.1 :center false})
-               (m/translate [0 0 (- height 0.1)]))))
+(defn- rotation-axis-and-angle [v1 v2 opp]
+  (let [cross (tf/cross v1 v2)]
+    (if (about-equal? cross [0 0 0])
+      (if (opposing? v1 v2)
+        [pi opp]
+        [0 opp])
+      [(angle-between v1 v2) (tf/normalise cross)])))
 
-(defn hull-shell
-  [bottom-shape top-shape height thickness]
-  (let [min (m/circle thickness)
-        inner-hull (hull bottom-shape top-shape height)
-        outer-hull (hull (m/minkowski min bottom-shape)
-                         (m/minkowski min top-shape)
-                         height)]
-    (m/difference outer-hull inner-hull)))
+(def identity-mat [[1 0 0] [0 1 0] [0 0 1]])
 
-(defmethod s/write-expr :bolt
-  [depth [_ type turns higbee_arc]]
-  (list (apply str (repeat depth " "))
-        "bolt(" type ",turns=" turns ",higbee_arc=" higbee_arc ");"))
-
-(defmethod s/write-expr :screw-by-pitch
-  [depth [_ pitch d0 dr length flat]]
-  (list (apply str (repeat depth " "))
-        "screwByPitch(" pitch ",d0=" d0 ",dr=" dr ",length=" length, ",flat=" flat ");\n"))
-
-(defmethod s/write-expr :screw-by-twist
-  [depth [_ twist d0 dr length flat]]
-  (list (apply str (repeat depth " "))
-        "screwByTwist(twist=" twist ",d0=" d0 ",dr=" dr ",length=" length, ",flat=" flat ");\n"))
-
-(defn to-array-string [v]
-  (format "[%s]"
-          (string/join ", "
-                       (map (fn [point]
-                              (format "[%s]" (string/join ", " point)))
-                            v))))
-
-(defmethod s/write-expr :helix-extrude
-  [depth [_ shape-pts radius levels level-dist vt-dir rt-dir twist scale triangles]]
-  (let [dyn-vars (str
-                  (when m/*fn* (str ", $fn=" m/*fn*))
-                  (when m/*fa* (str ", $fa=" m/*fa*))
-                  (when m/*fs* (str ", $fs=" m/*fs*)))]
-    (list (apply str (repeat depth " "))
-          "helix_extrude(shape_pts=" (to-array-string shape-pts) ", radius=" radius
-          ", levels=" levels ", level_dist=" level-dist
-          (format ", vt_dir=\"%s\"" vt-dir)
-          (format ", rt_dir=\"%s\"" rt-dir) ", twist=" twist
-          ", scale=" scale
-          (format ", triangles=\"%s\"" triangles)
-          dyn-vars
-          ");")))
-
-(defn bolt
-  ([type turns higbee-arc]
-   `(:bolt ~type ~turns ~higbee-arc))
-  ([type turns]
-   (bolt type turns 30))
-  ([type]
-   (bolt type 5)))
-
-(defn screw-by-pitch
-  ([& {:keys [pitch d0 dr length flat]
-       :or {pitch 3.6
-            d0 12
-            dr 1.5
-            length 12
-            flat 0.6}}]
-   `(:screw-by-pitch ~pitch ~d0 ~dr ~length ~flat)))
-
-(defn screw-by-twist
-  ([& {:keys [twist d0 dr length flat]
-       :or {twist (* 4 360)
-            d0 12
-            dr 1.5
-            length 12
-            flat 0.6}}]
-   `(:screw-by-twist ~twist ~d0 ~dr ~length ~flat)))
+(defn ->scad-transform
+  "Make an OpenSCAD transformation function between two coordinate frames offset by `pose`."
+  ([m pose] (->scad-transform identity-mat m pose))
+  ([a b pose]
+   (let [[angle ortho] (rotation-axis-and-angle (nth a 0) (nth b 0) [0 0 1])
+         c (rotate a ortho angle)
+         [angle-2 ortho-2] (rotation-axis-and-angle (nth b 1) (nth c 1) (nth c 0))]
+     (comp
+      (partial m/translate pose)
+      (partial m/rotatev (- angle-2) ortho-2)
+      (partial m/rotatev angle ortho)))))
